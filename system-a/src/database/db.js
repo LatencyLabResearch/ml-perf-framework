9e65de9dae322e39836a17052e16e22bcb487157
sweesram <146808341+sweesram@users.noreply.github.com>
2026-06-07 15:58:00 +0530

    Dev swess (#15)
    * fix: add SQLite WAL mode optimizations in db.js
    
    ---------
    
    Co-authored-by: vipusrihar <vipusrihar@gmail.com>

--- system-a/src/database/db.js
+++ system-a/src/database/db.js
@@ -5,4 +5,9 @@ const dbPath = path.join(__dirname, '../../data/workload.db');
 
 const db = new Database(dbPath);
 
+// Performance optimizations for concurrent load -heavy api
+db.pragma('journal_mode = WAL');      // ← allows concurrent reads & writes
+db.pragma('synchronous = NORMAL');    // ← faster writes, still safe
+db.pragma('cache_size = 10000');      // ← more memory cache
+db.pragma('temp_store = MEMORY');     // ← temp tables in memory
 module.exports = db;
